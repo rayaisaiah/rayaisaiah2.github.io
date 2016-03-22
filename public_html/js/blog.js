84cf797f792f9d2e6081462f94e91de0f6804429
Isiaiah Raya <rayaisaiah@gmail.com>
2016-03-22 11:31:45 -0700

    finished vid 9

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,7 +1,15 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(function () {
+    var APPLICATION_ID = "D124128E-C118-695A-FF7D-9D55700D0B00",
+        SECRET_KEY = "DDBBDCD1-AD79-A758-FF1B-F8A67286BD00",
+        VERSION = "vl";
+        
+        Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+        
+        var user = new Backendless.user();
+        user.email = "rayaisaiah@gmail.com";
+        user.password="Mittens12345";
+        Backendless.userService.register(user);
+    
+});
 
 
