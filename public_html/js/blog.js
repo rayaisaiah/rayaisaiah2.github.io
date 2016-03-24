84daaa217500af8be0de2226dcb4f2e55df6ca4a
Isiaiah Raya <rayaisaiah@gmail.com>
2016-03-24 09:53:06 -0700

    finished vid 10

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,15 +1,21 @@
 $(function () {
     var APPLICATION_ID = "D124128E-C118-695A-FF7D-9D55700D0B00",
         SECRET_KEY = "EFEBCA59-7F7D-ABEF-FFEC-21122F1B4A00",
-        VERSION = "vl";
+        VERSION = "v1";
         
         Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
         
-        var user = new Backendless.User();
-        user.email = "rayaisaiah@gmail.com";
-        user.password ="Mittens12345";
-        Backendless.UserService.register(user);
-    
+        var dataStore = Backendless.Persistence.of(Posts);
+        var post = new Post({title: "MY First Blog Post", content:"My First Blog Post Content", email:"email@email.com"});
+        dataStore.save(post);
+       
 });
+
+function Post(args){
+    args = args || {};
+    this.title = args.title || "";
+    this.content = args.content || "";
+    this.authorEmail = args.emailEmail || "";
+}
 
 
