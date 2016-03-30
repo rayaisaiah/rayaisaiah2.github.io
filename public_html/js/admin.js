d72c7c111e646bd62205eaab3bce4efbdeb78dbd
Isiaiah Raya <rayaisaiah@gmail.com>
2016-03-30 09:10:38 -0700

    finished vid 21

--- public_html/js/admin.js
+++ public_html/js/admin.js
@@ -31,7 +31,13 @@ function Posts(args){
     this.authorEmail = args.authorEmail || "";
 }
 
+function userLoggedIn() {
+    console.log("user successfully logged in");
+}
 
-
+function gotError(error) {
+    console.log("Error message - " + error.message);
+    console.log("Error code - " + error.code);
+}
 
 
